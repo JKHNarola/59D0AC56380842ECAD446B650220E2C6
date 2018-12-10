9fd438b95eaae6198790f6d66a221ab26fab64fb
Jigar <jkh@narola.email>
2018-12-10 18:23:10 +0530

    - ErrorPage Email template

--- src/lib/mailservice.js
+++ src/lib/mailservice.js
@@ -1,4 +1,7 @@
+var fs = require('fs');
+var path = require("path");
 const sgMail = require('@sendgrid/mail');
+
 exports.sendMail = function (to, subject, body, isText) {
     sgMail.setApiKey(global.appconfig.emailconfig.sendgridkey);
     var msg = {
@@ -36,48 +39,109 @@ exports.generateErrorBody = function (error, req) {
     return prepareErrorBody(error, req);
 };
 
+//TODO code cleanup
 var prepareErrorBody = function (error, req) {
-    var err = error.stack ? error.stack : error;
-    var str = "<html><head><title>Some error occured</title></head><body>";
-    str += "<div><h4>Error</h4></div>";
-    str += "<div>" + err + "</div>";
-    str += "<br/><br/>";
+    var str = "";
+    var errorTemplateFile = path.join(__dirname, "..", "views", "staticpages", "errortemplate.html");
+    if (fs.existsSync(errorTemplateFile))
+        str = fs.readFileSync(errorTemplateFile).toString();
 
-    if (req) {
-        str += "<div><h4>Request</h4></div>";
-        str += "<div><b>Url :</b> " + req.originalUrl + "</div>";
-        if (req.method) str += "<div><b>Method :</b> " + req.method + "</div>";
-        if (req.orgParams) {
-            var x = JSON.stringify(req.orgParams);
-            if (x && x.length > 1000) {
-                x = x.substring(0, 999) + "...";
-            }
-            str += "<div><b>Params :</b> " + x + "</div>";
-        } else if (req.params) {
-            x = JSON.stringify(req.params);
-            if (x && x.length > 1000) {
-                x = x.substring(0, 999) + "...";
-            }
-            str += "<div><b>Params :</b> " + x + "</div>";
-        }
+    if (str !== "") {
+        str = str.replace("[errmsg]", error.message ? error.message : "");
+        str = str.replace("[errstack]", error.stack ? error.stack : "");
+        str = str.replace("[reqmethod]", req && req.method ? req.method.toUpperCase() : "");
+        str = str.replace("[reqpath]", req && req.originalUrl ? req.originalUrl : "");
 
-        if (req.query) {
-            x = JSON.stringify(req.query);
-            if (x && x.length > 1000) {
-                x = x.substring(0, 999) + "...";
-            }
-            str += "<div><b>Query :</b> " + x + "</div>";
+        var body = req && req.body ? JSON.stringify(req.body) : "";
+        if (body && body.length > 1000) {
+            body = body.substring(0, 999) + "...";
         }
-        if (req.headers) {
-            x = JSON.stringify(req.headers);
-            if (x && x.length > 1000) {
-                x = x.substring(0, 999) + "...";
-            }
-            str += "<div><b>Headers :</b> " + x + "</div>";
+        str = str.replace("[reqbody]", body);
+
+        var query = req && req.query ? JSON.stringify(req.query) : "";
+        if (query && query.length > 1000) {
+            query = query.substring(0, 999) + "...";
         }
-        if (req.hostname) str += "<div>Hostname : " + req.hostname + "</div>";
-        if (req.ip) str += "<div><b>Ip :,</b> " + req.ip + "</div>";
+        str = str.replace("[reqquery]", query);
+
+        var params = req && req.orgParams ? JSON.stringify(req.orgParams) : "";
+        if (params === "") params = req && req.params ? JSON.stringify(req.params) : "";
+        if (params && params.length > 1000) {
+            params = params.substring(0, 999) + "...";
+        }
+        str = str.replace("[reqparam]", params);
+
+        var headers = req && req.headers ? JSON.stringify(req.headers) : "";
+        if (headers && headers.length > 1000) {
+            headers = headers.substring(0, 999) + "...";
+        }
+        str = str.replace("[reqheaders]", headers);
+
+        var hostname = req && req.hostname ? req.hostname : "";
+        if (hostname && hostname.length > 1000) {
+            hostname = hostname.substring(0, 999) + "...";
+        }
+        str = str.replace("[reqhostname]", hostname);        
+
+        var ip = req && req.ip ? req.ip : "";
+        if (ip && ip.length > 1000) {
+            ip = ip.substring(0, 999) + "...";
+        }
+        str = str.replace("[reqip]", ip);   
+
+        return str;
     }
+    else {
+        var err = error.stack ? error.stack : error;
+        str = "<html><head><title>Some error occured</title></head><body>";
+        str += "<div><h4>Error</h4></div>";
+        str += "<div>" + err + "</div>";
+        str += "<br/><br/>";
 
-    return str + "</body></html>";
+        if (req) {
+            str += "<div><h4>Request</h4></div>";
+            str += "<div><b>Url :</b> " + req.originalUrl + "</div>";
+            if (req.method) str += "<div><b>Method :</b> " + req.method + "</div>";
+            if (req.orgParams) {
+                var x = JSON.stringify(req.orgParams);
+                if (x && x.length > 1000) {
+                    x = x.substring(0, 999) + "...";
+                }
+                str += "<div><b>Params :</b> " + x + "</div>";
+            } else if (req.params) {
+                x = JSON.stringify(req.params);
+                if (x && x.length > 1000) {
+                    x = x.substring(0, 999) + "...";
+                }
+                str += "<div><b>Params :</b> " + x + "</div>";
+            }
+
+            if (req.query) {
+                x = JSON.stringify(req.query);
+                if (x && x.length > 1000) {
+                    x = x.substring(0, 999) + "...";
+                }
+                str += "<div><b>Query :</b> " + x + "</div>";
+            }
+
+            if (req.body) {
+                x = JSON.stringify(req.body);
+                if (x && x.length > 1000) {
+                    x = x.substring(0, 999) + "...";
+                }
+                str += "<div><b>Body :</b> " + x + "</div>";
+            }
+
+            if (req.headers) {
+                x = JSON.stringify(req.headers);
+                if (x && x.length > 1000) {
+                    x = x.substring(0, 999) + "...";
+                }
+                str += "<div><b>Headers :</b> " + x + "</div>";
+            }
+            if (req.hostname) str += "<div>Hostname : " + req.hostname + "</div>";
+            if (req.ip) str += "<div><b>Ip :,</b> " + req.ip + "</div>";
+        }
+        return str + "</body></html>";
+    }
 };

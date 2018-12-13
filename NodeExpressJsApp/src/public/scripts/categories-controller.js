731a4ca9346293ac92ecd1c42b9eacda3854ba9b
Jigar <jkh@narola.email>
2018-12-13 09:55:08 +0530

    - Solved image issue

--- NodeExpressJsApp/src/public/scripts/categories-controller.js
+++ NodeExpressJsApp/src/public/scripts/categories-controller.js
@@ -10,8 +10,7 @@ app.controller('categoriesCtrl', function ($scope, apiService, appUtils) {
             .then(function (r) {
                 if (r.status === 1) {
                     r.data.forEach(function (x) {
-                        x.picture = arrayBufferToBase64(x.picture.data);
-
+                        x.picture = appUtils.arrayBufferToBase64Image(x.picture.data);
                     });
                     $scope.data = r.data;
                 }
@@ -19,15 +18,5 @@ app.controller('categoriesCtrl', function ($scope, apiService, appUtils) {
             .catch(function (e) {
                 console.log(e);
             });
-    };
-
-    var arrayBufferToBase64 = function (buffer) {
-        var binary = '';
-        var bytes = new Uint16Array(buffer);
-        var len = bytes.byteLength;
-        for (var i = 0; i < len; i++) {
-            binary += String.fromCharCode(bytes[i]);
-        }
-        return window.btoa(binary);
     };
 });
